6c5a18f7626410005b73d508e18b25d223257844
Kooldeji <dejiakande33@gmail.com>
2018-12-14 12:39:52 +0100

    Update EmployeeManagement.java

--- src/com/example/hope/EmployeeManagement.java
+++ src/com/example/hope/EmployeeManagement.java
@@ -2,17 +2,28 @@ package com.example.hope;
 
 public class EmployeeManagement {
     private EmployeeDetail employeeDetail;
+    private Repository repository
 
     public EmployeeManagement(EmployeeDetail employeeDetail) {
         this.employeeDetail = employeeDetail;
+        repository = Repository.getInstance();
     }
 
     public void markPresent(){
         employeeDetail.setAttendanceCount(employeeDetail.getAttendanceCount()+1);
+        saveEmployee();
+    }
+    
+    public static EmployeeDetail resetAttendanceCount(EmployeeDetail employeeDetail){
+        employeeDetail.setAttendanceCount(0);
+        saveEmployee();
+    }
+    
+    private void saveEmployee(){
+        return repository.saveEmployeeDetail(employeeDetail);
     }
 
     public static EmployeeDetail registerEmployee(EmployeeDetail employeeDetail){
-        Repository repository = Repository.getInstance();
         return repository.saveEmployeeDetail(employeeDetail);
     }
 
